e8d11e32a83e6fb0d66f0b372ed8ba237bf651e5
Denis <dineresc@gmail.com>
2020-12-20 16:12:07 -0300

    corrige nome recurso endpoint

--- src/main/java/com/example/demoredis/controllers/PedidosController.java
+++ src/main/java/com/example/demoredis/controllers/PedidosController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/pedidos")
 public class PedidosController {
 
     @Resource
