0b58f67172022df155fa01cae9acae86494fe13c
Denis <dineresc@gmail.com>
2020-12-20 16:10:55 -0300

    criando controller para pedidos

---
+++ src/main/java/com/example/demoredis/controllers/PedidosController.java
@@ -0,0 +1,23 @@
+package com.example.demoredis.controllers;
+
+import com.example.demoredis.models.Pedido;
+import com.example.demoredis.services.PedidosService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/orders")
+public class PedidosController {
+
+    @Resource
+    PedidosService pedidosService;
+
+    @GetMapping("/{id}")
+    public Pedido getOrder(@PathVariable String id) {
+        return pedidosService.getPedidoById(id);
+    }
+}
